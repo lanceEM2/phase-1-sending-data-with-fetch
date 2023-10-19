2f54186c80a01061c525c307d2db3af2892049ff
Dylan <dylanlancekemboi@gmail.com>
2023-10-19 10:12:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(name, email) {
+    const object = {
+      name: name,
+      email: email
+    };
+  
+    fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(object)
+    })
+    .then(response => response.json())
+    .then(object => {
+      const id = object.id;
+      const idElement = document.createElement('p');
+      idElement.textContent = `New ID: ${id}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement('p');
+      errorElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+    });
+  }
+  
+  // Usage
+  submitData("John Doe", "johndoe@example.com");
+  
+  
